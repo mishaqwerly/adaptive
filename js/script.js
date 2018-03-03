491b44d31704d768a652982bee70e81c8be8249e
unknown <mishaqwerly>
2018-03-03 20:59:25 +0200

    test_4

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('.slider-slick').slick({
+		autoplaySpeed: 3000,
+		autoplay: true,
+		dots: false,
+		arrows: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',
+		infinite: true
+	});
+});
